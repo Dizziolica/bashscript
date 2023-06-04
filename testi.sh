23deb2898dbba1be01e1b58688a9826c116df546
Caroline Oliveira <104214541+Dizziolica@users.noreply.github.com>
2023-06-04 23:17:19 +0100

    Mostra a localização e a data

---
+++ testi.sh
@@ -0,0 +1,19 @@
+
+argone=$1
+argtwo=$2
+date=$(date)
+user=$(whoami)
+whereami=$(pwd)
+echo "Hello"
+
+echo "What's your name?"
+
+
+echo "Hello ${argone} ${argtwo}"
+
+
+echo "you are $user and you locate on $whereami in this date $date"
+
+
+
+
